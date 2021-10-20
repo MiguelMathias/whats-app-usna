d47075d9b9e570aae26e0e85720d020c84d17cb3
Miguel Mathias <m224212@usna.edu>
2021-10-20 10:04:44 -0400

    Restaurant Item Detail Page tweaks

--- gitscript.sh
+++ gitscript.sh
@@ -2,8 +2,8 @@ if [ "$1" == 'commit' ]
 then
     git add .
     git commit -m "$2"
-    git pull remote origin
-    git push remote origin --all
+    git pull origin
+    git push origin --all
 elif [ "$1" == 'pull' ]
 then
     git pull origin
