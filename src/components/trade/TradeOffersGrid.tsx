ac5578820f98234bac7871f142d5bfcfbe302e8d
Miguel Mathias <titomathias73@gmail.com>
2022-04-18 14:44:52 -0400

    Trade Offers Grid responsive now

--- src/components/trade/TradeOffersGrid.tsx
+++ src/components/trade/TradeOffersGrid.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffectOnce } from 'react-use'
 import { TradeCategoryModel, TradeOfferModel } from '../../data/trade/Trade'
 import TradeOfferCard from './TradeOfferCard'
 import './TradeOffersGrid.scss'
@@ -10,7 +11,40 @@ type TradeOffersGridProps = {
 	categories?: TradeCategoryModel[]
 }
 
+type WindowSize = 'sm' | 'md' | 'lg'
+
+const getSize = (): WindowSize => (window.innerWidth > 800 ? 'lg' : window.innerWidth > 600 ? 'md' : 'sm')
+const getCols = (windowSize: WindowSize) => (windowSize === 'lg' ? 3 : windowSize === 'md' ? 2 : 1)
+
+const getGridCols = (arr: any[], cols: number) => {
+	function getEveryNth(arr: any[], nth: number, start: number = 0) {
+		const result = []
+
+		for (let i = start; i < arr.length; i += nth) {
+			result.push(arr[i])
+		}
+
+		return result
+	}
+	const grid: any[][] = []
+	for (let i = 0; i < cols; i++) {
+		grid.push(getEveryNth(arr, cols, i))
+	}
+
+	return grid
+}
+
 const TradeOffersGrid: React.FC<TradeOffersGridProps> = ({ isMine, tradeOffers, searchText, categories }) => {
+	const [windowSize, setWindowSize] = useState<WindowSize>(getSize())
+
+	useEffectOnce(() => {
+		const handleResize = () => setWindowSize(getSize())
+
+		window.addEventListener('resize', handleResize)
+
+		return () => window.removeEventListener('resize', handleResize)
+	})
+
 	const shoudDisplay = (tradeOffer: TradeOfferModel) => {
 		if (categories?.includes(tradeOffer.category) || !categories) {
 			if (searchText?.length)
@@ -23,20 +57,19 @@ const TradeOffersGrid: React.FC<TradeOffersGridProps> = ({ isMine, tradeOffers, 
 		}
 		return false
 	}
+
 	return tradeOffers.length <= 0 ? (
 		<div style={{ minHeight: '100%', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
 			<p>No offers right now! Make one, or come back later.</p>
 		</div>
 	) : (
-		<div className='row'>
-			{tradeOffers.map((tradeOffer, i) => (
-				<React.Fragment key={i}>
-					{shoudDisplay(tradeOffer) && (
-						<div className='col'>
-							<TradeOfferCard isMine={isMine} tradeOffer={tradeOffer} />
-						</div>
-					)}
-				</React.Fragment>
+		<div className='row' style={{ marginBottom: 10 }}>
+			{getGridCols(tradeOffers, getCols(windowSize)).map((tradeOffers, i) => (
+				<div key={i} className='col'>
+					{tradeOffers.map((tradeOffer, j) => (
+						<React.Fragment key={j}>{shoudDisplay(tradeOffer) && <TradeOfferCard isMine={isMine} tradeOffer={tradeOffer} />}</React.Fragment>
+					))}
+				</div>
 			))}
 		</div>
 	)
